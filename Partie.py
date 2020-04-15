6e5edcff2531f76f65cdd74d6fd9d2afe9c93bab
antoine-13 <62340674+antoine-13@users.noreply.github.com>
2020-04-15 21:31:45 +0200

    Add files via upload

--- Partie.py
+++ Partie.py
@@ -1,44 +1,58 @@
+###########################################################################################
+#                                                                                         #
+#                                                                                         #
+#                                      Classe Partie:                                     #
+#                                                                                         #
+#                           gère le placement des pions, la                               #
+#                           vérification des coups, la définition des joueurs             #
+#                                                                                         #
+#                                                                                         #
+###########################################################################################
+
+
 class Partie:
+
+    #Initialisation des attributs de Partie 
     def __init__(self):
-        super().__init__()
         self.tab = None 
         self.nbr_joueurs = 2
         self.tour = 1
         self.joueurs_du_tour = [1] * 1
         self.non_joueurs = [1] * 3
 
+    #Méthode joueurs qui définie qui joue et qui ne joue pas en fonction du numéro du tour 
     def joueurs(self):
         type_pion = self.tour % self.nbr_joueurs
         
-        if type_pion == 1:
+        if type_pion == 1:                      #Si c'est au tour de 1 de jouer on ajoute son symbole au tableau des joueurs et on ajoute les reste des joueurs au tableau des non joueurs
             self.joueurs_du_tour[0] = " X "
             self.non_joueurs[0] = " 0 "
             self.non_joueurs[1] = " Y "
             self.non_joueurs[2] = " U "
-        elif type_pion == 0:
+        elif type_pion == 0:                     #Si c'est au tour de 0 de jouer on ajoute son symbole au tableau des joueurs et on ajoute les reste des joueurs au tableau des non joueurs
             self.joueurs_du_tour[0] = " 0 "
             self.non_joueurs[0] = " X "
             self.non_joueurs[1] = " Y "
             self.non_joueurs[2] = " U "
            
-        elif type_pion == 2:
+        elif type_pion == 2:                     #Si c'est au tour de 2 de jouer on ajoute son symbole au tableau des joueurs et on ajoute les reste des joueurs au tableau des non joueurs
             self.joueurs_du_tour[0] = " Y "
             self.non_joueurs[0] = " 0 "
             self.non_joueurs[1] = " X "
             self.non_joueurs[2] = " U "
            
-        elif type_pion == 3:
+        elif type_pion == 3:                     #Si c'est au tour de 3 de jouer on ajoute son symbole au tableau des joueurs et on ajoute les reste des joueurs au tableau des non joueurs
             self.joueurs_du_tour[0] = " U "
             self.non_joueurs[0] = " 0 "
             self.non_joueurs[1] = " Y "
             self.non_joueurs[2] = " X "
             
-
+    #Méthode qui demande à l'utilisateur ou il souhaite jouer
     def coup(self):
         coord = input("Ou souhaitez vous jouer ? (Saisir les coordonnées séparer pas un \';\') ").split(";",)
-        return coord
+        return coord                                                                                                    #retourne dans un tableau les coordonées du choix
     
-    
+    #Méthode qui vérifie si la case est vide ou non 
     def validite_case(self,coup, tableau):
         #récupération des coordonées ou le joueur souhaite poser son pion 
         cl = int(coup[0])
@@ -46,234 +60,77 @@ class Partie:
        
         #vérification si la case est déjà prise par un autre pion
         if tableau[lg][cl] == " X " or tableau[lg][cl] == " 0 ":
-            return 1
+            return 1                                                        #On retourne 1
         else:
-            return 0
+            return 0                                                        #Sinon on retourne 0
 
     def validite_pos(self, coup, tableau):
         #récupération des coordonées ou le joueur souhaite poser son pion 
         cl = int(coup[0])
         lg = int(coup[1])
-        a = self.joueurs_du_tour[0]
-        b = self.non_joueurs[0]
-        c = self.non_joueurs[1]
-        d = self.non_joueurs[2]
+        possibilite = 0
+        lg_add = [0, -1, +1]
+        cl_add = [0, -1, +1]
+        valid_dir_lg = []
+        valid_dir_cl = []
 
-        for b in self.non_joueurs:                       
-            if tableau[lg][cl + 1] == b or tableau[lg][cl + 1] == c or tableau[lg][cl + 1] == d:  #droite du pion joué
-                ncl = cl + 1
-                while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                    ncl += 1
-                if tableau[lg][ncl] == a:
-                    return 1
-
-            if tableau[lg][cl - 1] == b or tableau[lg][cl - 1] == c or tableau[lg][cl - 1] == d:  #gauche du pion joué
-                ncl = cl - 1
-                while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                    ncl -= 1
-                if tableau[lg][ncl] == a:
-                    return 1
-
-            if tableau[lg - 1][cl] == b or tableau[lg - 1][cl] == c or tableau[lg - 1][cl] == d:  #en haut du pion joué
-                nlg = lg - 1
-                while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                    nlg -= 1
-                if tableau[nlg][cl] == a:
-                    return 1
-            
-            if tableau[lg + 1][cl] == b or tableau[lg + 1][cl] == c or tableau[lg + 1][cl] == d:  #en bas du pion joué
-                nlg = lg + 1
-                while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                    nlg += 1
-                if tableau[nlg][cl] == a:
-                    return 1
-
-            if tableau[lg + 1][cl + 1] == b or tableau[lg + 1][cl + 1] == c or tableau[lg + 1][cl + 1] == d:  #en bas a droite du pion joué
-                nlg = lg + 1
-                ncl = cl + 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg += 1
-                    ncl += 1
-                if tableau[nlg][ncl] == a:
-                    return 1
-
-            if tableau[lg + 1][cl - 1] == b or tableau[lg + 1][cl - 1] == c or tableau[lg + 1][cl - 1] == d:  #en bas a gauche du pion joué
-                nlg = lg + 1
-                ncl = cl - 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg += 1
-                    ncl -= 1
-                if tableau[nlg][ncl] == a:
-                    return 1
-
-            if tableau[lg - 1][cl - 1] == b or tableau[lg - 1][cl - 1] == c or tableau[lg - 1][cl - 1] == d:  #en haut a gauche du pion joué
-                nlg = lg - 1
-                ncl = cl - 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg -= 1
-                    ncl -= 1
-                if tableau[nlg][ncl] == a:
-                    return 1
-
-            if tableau[lg - 1][cl + 1] == b or tableau[lg - 1][cl + 1] == c or tableau[lg - 1][cl + 1] == d:  #en haut a gauche du pion joué
-                nlg = lg - 1
-                ncl = cl + 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg -= 1
-                    ncl += 1
-                if tableau[nlg][ncl] == a:
-                    return 1
-
-            
-        return 0
+        #On vérifie si dans toute les direction la case à coté contient un pion autre que celui qui joue
+        #Si c'est le cas on continue dans la meme direction tant que le pion est différent de celui qui joue
+        # Si on fini par croiser un pion correspondant à celui qui joue
+        #On renvoi 1 pour dire que le coup est valable vue qu'il encadre bien d'autres pions 
         
-
+        for a in lg_add:  
+            for b in cl_add:                
+                if tableau[lg + a][cl + b] in self.non_joueurs:
+                    nlg = lg + a
+                    ncl = cl + b
+                    while tableau[nlg][ncl] in self.non_joueurs:
+                        ncl += b
+                        nlg += a
+                        if tableau[nlg][ncl] in self.joueurs_du_tour:
+                            valid_dir_lg.append(a)
+                            valid_dir_cl.append(b)
+                            possibilite += 1
+                            
+                        
+        return possibilite, valid_dir_lg, valid_dir_cl
+            
+                                                            
+        
+    #Méthode ajout_pion qui ajoute un pion à la position souaitez par l'utilisateur
     def ajout_pion(self, coup, tableau):
-
-            tableau[int(coup[1])][int(coup[0])] = self.joueurs_du_tour[0]
-            self.tab = tableau
-
+            tableau[int(coup[1])][int(coup[0])] = self.joueurs_du_tour[0]           #ajoute la valeur du pion qui joue à l'emplacement souhaitez
+            self.tab = tableau                                                      #Modifie le tableau final
 
 
 
-    def inversement(self, tableau, coup):
+
+    def inversement(self, tableau, coup, dir_lg, dir_cl):
+        #récupération des coordonnées du coup
         cl = int(coup[0])
         lg = int(coup[1])
-        a = self.joueurs_du_tour[0]
-        b = self.non_joueurs[0]
-        c = self.non_joueurs[1]
-        d = self.non_joueurs[2]
+
         
-        if tableau[lg][cl] == a:                          #droite du pion joué
-            if tableau[lg][cl + 1] == b or tableau[lg][cl + 1] == c or tableau[lg][cl + 1] == d:
-                ncl = cl + 1
-                while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                    ncl += 1
-                    if tableau[lg][ncl] == a:
-                        ncl = cl + 1
-                        while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                            tableau[lg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = lg
-                            self.inversement(tableau, pion)
-                            ncl += 1
+
+
+        #On vérifie si dans toute les direction la case à coté contient un pion autre que celui qui joue
+        #Si c'est le cas on continue dans la meme direction tant que le pion est différent de celui qui joue
+        # Si on fini par croiser un pion correspondant à celui qui joue
+        #On renvoi 1 pour dire que le coup est valable vue qu'il encadre bien d'autres pions 
+        for i in range (0, len(dir_cl)):
+            nlg = lg + dir_lg[i]
+            ncl = cl + dir_cl[i]
+            while tableau[nlg][ncl] in self.non_joueurs:
+                pion = [1] * 2
+                pion[0] = ncl
+                pion[1] = nlg
+                self.ajout_pion(pion, tableau)
+                possibilite, valid_dir_lg, valid_dir_cl = self.validite_pos(pion, tableau)
+                self.inversement(tableau, pion, valid_dir_lg, valid_dir_cl)
+                nlg += dir_lg[i]
+                ncl += dir_cl[i]
 
             
-            if tableau[lg][cl - 1] == b or tableau[lg][cl - 1] == c or tableau[lg][cl - 1] == d:                     #gauche du pion joué            
-                ncl = cl - 1
-                while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                    ncl -= 1
-                    if tableau[lg][ncl] == a:
-                        ncl = cl - 1
-                        while tableau[lg][ncl] == b or tableau[lg][ncl] == c or tableau[lg][ncl] == d:
-                            tableau[lg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = lg
-                            self.inversement(tableau, pion)
-                            ncl -= 1
-            
-
-            if tableau[lg + 1][cl] == b or tableau[lg + 1][cl] == c or tableau[lg + 1][cl] == d:        #En bas du pion joué 
-                nlg = lg + 1
-                
-                while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                    nlg += 1
-                    if tableau[nlg][cl] == a:
-                        nlg = lg + 1
-                        while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                            tableau[nlg][cl] = a
-                            pion = [1] * 2
-                            pion[0] = cl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg += 1
-
-            if tableau[lg - 1][cl] == b or tableau[lg - 1][cl] == c or tableau[lg - 1][cl] == d:            #En haut du pion joué 
-                nlg = lg - 1
-                while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                    nlg -= 1
-                    if tableau[nlg][cl] == a:
-                        nlg = lg - 1
-                        while tableau[nlg][cl] == b or tableau[nlg][cl] == c or tableau[nlg][cl] == d:
-                            tableau[nlg][cl] = a
-                            pion = [1] * 2
-                            pion[0] = cl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg -= 1
-
-            if tableau[lg + 1][cl + 1] == b or tableau[lg + 1][cl + 1] == c or tableau[lg + 1][cl + 1] == d:            #En bas à droite du pion joué 
-                nlg = lg + 1
-                ncl = cl + 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg += 1
-                    ncl += 1
-                    if tableau[nlg][ncl] == a:
-                        nlg = lg + 1
-                        ncl = cl + 1
-                        while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                            tableau[nlg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg += 1
-                            ncl += 1
-
-            if tableau[lg - 1][cl - 1] == b or tableau[lg - 1][cl - 1] == c or tableau[lg - 1][cl - 1] == d:            #En haut à gauche du pion joué 
-                nlg = lg - 1
-                ncl = cl - 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg -= 1
-                    ncl -= 1
-                    if tableau[nlg][ncl] == a:
-                        nlg = lg - 1
-                        ncl = cl - 1
-                        while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                            tableau[nlg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg -= 1
-                            ncl -= 1
-
-            if tableau[lg - 1][cl + 1] == b or tableau[lg - 1][cl + 1] == c or tableau[lg - 1][cl + 1] == d:            #En haut à droite auche du pion joué 
-                nlg = lg - 1
-                ncl = cl + 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    ncl += 1
-                    if tableau[nlg][ncl] == a:
-                        nlg = lg - 1
-                        ncl = cl + 1
-                        while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                            tableau[nlg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg -= 1
-                            ncl += 1
-            
-            if tableau[lg + 1][cl - 1] == b or tableau[lg + 1][cl - 1] == c or tableau[lg + 1][cl - 1] == d:            #En bas à gauche auche du pion joué 
-                nlg = lg + 1
-                ncl = cl - 1
-                while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                    nlg += 1
-                    ncl -= 1
-                    if tableau[nlg][ncl] == a:
-                        nlg = lg + 1
-                        ncl = cl - 1
-                        while tableau[nlg][ncl] == b or tableau[nlg][ncl] == c or tableau[nlg][ncl] == d:
-                            tableau[nlg][ncl] = a
-                            pion = [1] * 2
-                            pion[0] = ncl
-                            pion[1] = nlg
-                            self.inversement(tableau, pion)
-                            nlg += 1
-                            ncl -= 1
                 
 
     
--- main.py
+++ main.py
@@ -1,56 +1,74 @@
+###########################################################################################
+#                                                                                         #
+#                                                                                         #
+#                                         Classe Main:                                    #
+#                                                                                         #
+#                           Rassemble les classes et gère la partie                       #
+#                                                                                         #
+#                                                                                         #
+#                                                                                         #
+###########################################################################################
+
+#Importation des librairies
 from Plateau import* 
 from Partie import*
 from menu import*
 import webbrowser
 
-
+#définition du main 
 def main():
+
+    #création des objets
     p = Partie()
     pl = Plateau()
     m = menu()
+
+    #définition du choix à 0
     choix = 0
 
+    #Tant que le choix n'est pas de lancer une partie 
     while choix != 1:
-        m.affichage_main_menu()
-        choix = m.user_choix()
+        m.affichage_main_menu()             #On affiche le menu
+        choix = m.user_choix()              #On demande à l'utilisateur son choix
         
-        if choix == 2:
-            pl.dimenssion = m.taille()
-        elif choix == 3:
-           p.nbr_joueurs = m.nbr_joueur()
-        elif choix == 4:
-            webbrowser.open("http://www.ffothello.org/othello/regles-du-jeu/")
+        if choix == 2:                      #Si son choix est de changer la taille
+            pl.dimenssion = m.taille()          #On lui demande la taille qu'il veut
+        elif choix == 3:                    #Si son choix est de changer le nombre de joueurs
+           p.nbr_joueurs = m.nbr_joueur()          #On lui demande le nombre de joueurs
+        elif choix == 4:                    #Si il veut connaitre les règles 
+            webbrowser.open("http://www.ffothello.org/othello/regles-du-jeu/")  #On le redirige vers la page officiel avec les règles
         
-        print("")
+        print("")   #saut de ligne
 
-    
+    #Une fois qu'il à lancer une partie on initialise la partie
     p.tab = pl.initialisation(nbr_joueurs=p.nbr_joueurs)
 
-
+    #tant que la fin de partie n'est vérifié
     while pl.test(p.tab) == 0:
-        if pl.joueur_eliminer(p.tour, p.nbr_joueurs) == 0 :
-            p.joueurs()
-            pl.affiche(p.tab, pl.dimenssion)
-            print("C'est à", p.joueurs_du_tour[0], "de jouer !")
-            a = p.coup()
+        p.joueurs()                                         #On définie qui joue 
+        if pl.joueur_eliminer(p.tour, p.nbr_joueurs) == 0 : #Si le joueur n'est pas éliminé
+            pl.affiche(p.tab, pl.dimenssion)                    #On affiche le plateau de jeu
+            print("C'est à", p.joueurs_du_tour[0], "de jouer !")            #On affiche le nom du joueur qui doit jouer
+            a = p.coup()                                                    #On lui demande ou il veux jouer 
         
         
-            if p.validite_case(a, p.tab) == 0:
-                if p.validite_pos(a, p.tab) == 1:
-                    p.ajout_pion(a, p.tab)
-                    p.inversement(p.tab, a)
-                    p.tour += 1
-                else:
-                    print("Merci de choisir un emplacement correct")
+            if p.validite_case(a, p.tab) == 0:                      #Si la case est vide
+                possibilite, valid_dir_lg, valid_dir_cl = p.validite_pos(a, p.tab)                    
+                if possibilite >= 1:                       #Si le coup est valide (qu'il retourne bien un pion adverse)
+                    p.ajout_pion(a, p.tab)                                  #On ajoute son pion
+                    p.inversement(p.tab, a, valid_dir_lg, valid_dir_cl)                                 #On retourne les pions encadrés 
+                    p.tour += 1                                             #On passe au tour suivant 
+                else:                                                   #Sinon
+                    print("Merci de choisir un emplacement correct")        #On lui demande de choisir une position correcte
 
-            else:
-                print("Cette emplacement est déjà pris !")
+            else:                                                   #Sinon
+                print("Cette emplacement est déjà pris !")              #On lui dis que la case est deja occupé
 
-        else:
+        else:                                           #Sinon on saute son tour 
             p.tour +=1
             
         
-
+    #Quand la partie prend fin on affiche le tableau final
     pl.affiche(p.tab,pl.dimenssion)
 
 
